17ffb0fa30609499c5e8734b55aa292cba1df1ae
Mohamed Sherif <Mohamed Sherif@DESKTOP-AR3TO2V>
2024-12-24 14:44:13 +0300

    Add project files.

---
+++ C# Basic5/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace C__Basic5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1- Write a program that prints an identity matrix using for loop, in other words takes a value n from the user and shows the identity table of size n * n.
+            #endregion
+
+            #region 2-Write a program in C# Sharp to find the sum of all elements of the array.
+
+            //int[] numbers = { 1, 2, 3, 4, 5, };
+            //int sum = 0;
+            //for (int i = 0; i < numbers.Length; i++) { 
+
+            //    sum += numbers[i];
+
+            //}
+            //Console.WriteLine(sum); 
+
+
+            #endregion
+
+            #region 3-Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
+            //int[] numbers = { 10, 20, 50, 100, 500 };
+            //Array.Sort(numbers);
+            //Console.WriteLine(numbers);
+
+            #endregion
+
+            #region 4-Write a program in C# Sharp to count the frequency of each element of an array.
+
+            #endregion
+
+            #region 5-Write a program in C# Sharp to find maximum and minimum element in an array
+            //int[] numbers = new int[10];
+            //for (int i = 0; i < numbers.Length; i++) {
+            //    Console.WriteLine($"Enter Value {i + 1}");
+            //    int.TryParse(Console.ReadLine(), out numbers[i]);
+
+            //}
+
+            //Array.Sort(numbers);
+
+            //Console.WriteLine(" Min " + numbers[0] + " Max " + numbers[numbers.Length-1]);
+
+
+            #endregion
+
+            #region 6-Write a program in C# Sharp to find the second largest element in an array.
+            //int[] numbers = new int[10];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine($"Enter Value {i + 1}");
+            //    int.TryParse(Console.ReadLine(), out numbers[i]);
+
+            //}
+
+            //Array.Sort(numbers);
+
+            //Console.WriteLine( $" Second Largest { numbers[numbers.Length - 2]}");
+            #endregion
+
+            #region 10-Write a program to create two multidimensional arrays of same size. Accept values from the user and store them in first array. Now copy all the elements of the first array on second array and print second array.
+            //int[,] marks = new int[2, 4]; // 8
+
+            //int[,] marksCopy = new int[2, 4];
+
+            //for (int i = 0; i < marks.Length; i++)
+            //{
+            //    int.TryParse(Console.ReadLine(), out marks[i / marks.GetLength(1), i % marks.GetLength(1)]);
+            //    //marks[i/marks.GetLength(1) , i % marks.GetLength(1)] = mark;
+            //}
+            //Console.Clear();
+            //Array.Copy(marks, 0, marksCopy, 0,8);
+            //foreach (int i in marksCopy) { 
+            //  Console.WriteLine(i);
+
+            //}
+
+
+
+
+
+
+            #endregion
+            #region 11-	Write a Program to Print One Dimensional Array in Reverse Order
+            //int[] numbers = new int[10];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine($"Enter Value {i + 1}");
+            //    int.TryParse(Console.ReadLine(), out numbers[i]);
+
+            //}
+
+            //foreach (var item in numbers)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Array.Reverse(numbers);
+            //foreach (var item in numbers)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+
+
+        }
+    }
+}
